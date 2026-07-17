9e25f7640ed14294cd876a70742eadac18cae7e8
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-17 10:23:12 +0700

    add Midtrans payment fields

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,10 +9,26 @@ class Payment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['booking_id', 'payment_method', 'amount', 'payment_status', 'transaction_code'];
+    protected $fillable = [
+        'booking_id',
+        'payment_method',
+        'amount',
+        'payment_status',
+        'transaction_code',
+        'order_id',
+        'snap_token',
+        'redirect_url',
+        'transaction_id',
+        'transaction_status',
+        'fraud_status',
+        'paid_at',
+        'raw_notification',
+    ];
 
     protected $casts = [
         'amount' => 'decimal:2',
+        'paid_at' => 'datetime',
+        'raw_notification' => 'array',
     ];
 
     public function booking()
